c1e280cc147b9709c4cf8da372fb63d9e83180a8
Munkhbaatar <gmunkhbaatarmn@gmail.com>
2015-05-13 11:04:33 +0900

    version 0.0.8 declared

--- natrix.py
+++ natrix.py
@@ -19,7 +19,7 @@ from jinja2 import Environment, FileSystemLoader
 sys.path.append("./packages")
 info, taskqueue  # pyflakes fix
 
-__version__ = "0.0.7+"
+__version__ = "0.0.8"
 
 
 # Core classes
